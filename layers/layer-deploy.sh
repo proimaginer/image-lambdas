440d07593da202b76543f1e6e0d839a0ec7fcbcf
이정준 <leejj90@naver.com>
2022-07-18 18:13:23 +0900

    리사이즈 & webp 변환

--- layers/layer-deploy.sh
+++ layers/layer-deploy.sh
@@ -3,5 +3,7 @@
 # 스테이지를 파라미터로 받아 배포 한다.
 cd ./modules/nodejs
 npm run update
+rm -rf ./node_modules/sharp
+SHARP_IGNORE_GLOBAL_LIBVIPS=1 npm install --arch=x64 --platform=linux --libc=glibc sharp
 cd ../..
 sls deploy --stage $1
--- src/gallery/setImg.js
+++ src/gallery/setImg.js
@@ -1,16 +1,52 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const Sharp = require('sharp');
+
+const s3 = new AWS.S3();
+const formats = ['jpg', 'jpeg', 'png'];
+
 /**
  * gallery 리사이즈 이미지 생성
  * @param event
  */
 exports.handler = async (event) => {
   try {
-    console.log(event);
-    console.log(event.Records);
+    const record = event.Records[0];
+
+    // 원본 이미지 가져오기
+    const originImage = await s3.getObject({
+      Bucket: record.s3.bucket.name,
+      Key: record.s3.object.key,
+    }).promise();
+
+    // 이미지에서 정보를 추출한다.
+    const keys = record.s3.object.key.split('/');
+    const [fileName, format] = keys[4].split('.');
+
+    // 유효한 포멧이 아닐 경우
+    if (!formats.includes(format)) {
+      console.log('[Format Error]', format);
+      return false;
+    }
+
+    // 리사이즈 및 webp 변환
+    const image = await Sharp(originImage.Body)
+      .resize(100)
+      .webp()
+      .toBuffer();
+
+    // 변환 이미지 업로드
+    const result = await s3.putObject({
+      Bucket: record.s3.bucket.name,
+      Key: `${keys[0]}/${keys[1]}/img/${keys[3]}/${fileName}.webp`,
+      Body: image,
+    }).promise();
+    console.log('[Put Success]', JSON.stringify(result, null, 2));
+
     return true;
   } catch (e) {
-    console.error(e);
+    console.error('[Error]', e);
     return false;
   }
 };
